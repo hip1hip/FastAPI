01672fe9e29db2ff831aafcac62264e71eb00c49
hip1hip <honginpyo2100@gmail.com>
2026-01-15 20:43:35 +0900

    fix. DB 테이블 생성 완료 , 그거에 맞게 모델 수정

--- app/models/board_model.py
+++ app/models/board_model.py
@@ -15,7 +15,7 @@ class BoardBase(SQLModel):
 class Board(BoardBase, TimestampModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     # Foreign Key 설정 (@ManyToOne)
-    user_id: int = Field(foreign_key="boarduser.id")
+    user_id: int = Field(foreign_key="board_user.id")
 
     # 관계 설정: author를 옹해 유저 객체에 바로 접근 가능
     writer: "BoardUser" = Relationship(back_populates="boards")
--- app/models/board_user_model.py
+++ app/models/board_user_model.py
@@ -9,10 +9,13 @@ if TYPE_CHECKING:
 
 # 1. 공통 필드 정의 (Pydantic 모델 역할)
 class BoardUserBase(SQLModel):
-    nick_name: str = Field(index=True)
+    nick_name: str = Field(index=False)
 
 
 class BoardUser(BoardUserBase, TimestampModel, table=True):
+    # 테이블 이름 명시
+    __tablename__: str = "board_user"
+
     id: Optional[int] = Field(default=None, primary_key=True)
 
     # @OneToMany
