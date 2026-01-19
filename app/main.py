6edf5284a82fd716fb688d54ffcc5592378cc9be
hip1hip <honginpyo2100@gmail.com>
2026-01-19 20:11:37 +0900

    fix. DB 테이블, 모델에 맞게 생성 하는 함수 메인에서 실행하도록 수정

--- app/main.py
+++ app/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from app.api.v1.router import api_router
 from fastapi.middleware.cors import CORSMiddleware
 
+from app.core.database import create_db_and_tables
+
 app = FastAPI()
 
 app.include_router(api_router, prefix="/api/v1")
@@ -14,6 +16,12 @@ app.add_middleware(
     allow_methods=["*"],  # 모든 HTTP 메서드(GET, POST 등) 허용
     allow_headers=["*"],  # 모든 헤더 허용
 )
+
+
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
+    print("DB 테이블 생성 시도 완료!")
 
 
 def main():
