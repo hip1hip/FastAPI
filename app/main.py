566793c2aee66d1d58f6bb1b31c098cf46f5081b
hip1hip <honginpyo2100@gmail.com>
2026-01-19 15:07:10 +0900

    feat. CORS 설정, 임시로 열어둠.

--- app/main.py
+++ app/main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
 from app.api.v1.router import api_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 app.include_router(api_router, prefix="/api/v1")
+
+# CORS 설정
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 모든 도메인 허용 (임시)
+    allow_credentials=True,
+    allow_methods=["*"],  # 모든 HTTP 메서드(GET, POST 등) 허용
+    allow_headers=["*"],  # 모든 헤더 허용
+)
 
 
 def main():
