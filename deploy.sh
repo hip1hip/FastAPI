2f2856ec7f962fa197bd8e8a8a2262287b54e96a
hip1hip <honginpyo2100@gmail.com>
2026-01-19 15:27:13 +0900

    fix. 라인 정리.

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 cd /home/ubuntu/workspace/FastAPI
-
 echo "1. 프로젝트 폴더로 이동 완료"
 
-git pull origin main
 
+git pull origin main
 echo "2. 최신 코드 pull 완료"
+
 
 CURRENT_PID=$(pgrep -f "uvicorn main:app")
 if [ -z "$CURRENT_PID" ]; then
@@ -14,12 +14,11 @@ else
     kill -15 $CURRENT_PID
     sleep 2 
 fi
-
 echo "3. 프로세스 정리 완료"
+
 
 nohup uv run fastapi run app/main.py --host 0.0.0.0 --port 8000 \
     > /dev/null 2>&1 < /dev/null &
-
 echo "4. 서버 백그라운드 실행 완료!"
 
 #chmod +x deploy.sh -> 파일 권한 수정
